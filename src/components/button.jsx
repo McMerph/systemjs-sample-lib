36418d96fef725ab37a18baa6ce4011ad22a2986
Aleksandr Sidorenko <asidorenko@simdaq.com>
2020-04-10 21:43:29 +1000

    Add `onClick` handler to button

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Button = ({ children }) => (
-  <button type="button">|UI lib|{children}</button>
+const Button = ({ children, onClick }) => (
+  <button type="button" onClick={onClick}>
+    |UI lib|{children}
+  </button>
 )
 Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
   children: PropTypes.oneOfType([
     PropTypes.string.isRequired,
     PropTypes.number.isRequired,
