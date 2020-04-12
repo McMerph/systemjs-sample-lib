aca76cf374710d750e207839e798fba4b3484754
Aleksandr Sidorenko <asidorenko@simdaq.com>
2020-04-12 16:51:08 +1000

    Use `webpack-dev-server` in dev script

--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,15 @@ module.exports = systemjsInterop.modifyWebpackConfig({
     libraryTarget: 'system',
   },
   devtool: 'source-map',
+  devServer: {
+    port: 3000,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers':
+        'X-Requested-With, content-type, Authorization',
+    },
+  },
   plugins: [new CleanWebpackPlugin()],
 })
 
